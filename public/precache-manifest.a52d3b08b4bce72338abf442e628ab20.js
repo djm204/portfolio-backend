7c42682df29d40ec306cb964901b9e9bd6dfe169
David Mendez <davidjm204@gmail.com>
2020-02-03 21:55:54 -0600

    initial commit, laravel scaffoleded
    laravel loads react app now

---
+++ public/precache-manifest.a52d3b08b4bce72338abf442e628ab20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6730123e621d2a6da0735d66c6279c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a830ea4fce01a9291cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e0985bdc95b1f1820208",
+    "url": "/static/js/2.49c17e64.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.49c17e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a830ea4fce01a9291cf",
+    "url": "/static/js/main.8c5dfb44.chunk.js"
+  },
+  {
+    "revision": "dc30ed6a687eddfe4a09",
+    "url": "/static/js/runtime-main.ebb9d084.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
