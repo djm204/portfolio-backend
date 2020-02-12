ea0fcb97975d478fa4436ee9b7d7d55e68d13a1f
David Mendez <desiratech@gmail.com>
2020-02-12 13:30:14 -0600

    forgot to add new assets... derp

---
+++ public/precache-manifest.1dddc479d16eba046e37947436dad781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d18bb21a0828f11c729ff0e82723db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d37ea7416650a02f63e",
+    "url": "/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "a087fc425722d5b1762f",
+    "url": "/static/css/main.a58a4749.chunk.css"
+  },
+  {
+    "revision": "8d37ea7416650a02f63e",
+    "url": "/static/js/2.82ff64ee.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.82ff64ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a087fc425722d5b1762f",
+    "url": "/static/js/main.c969e0fd.chunk.js"
+  },
+  {
+    "revision": "dc30ed6a687eddfe4a09",
+    "url": "/static/js/runtime-main.ebb9d084.js"
+  }
+]);
